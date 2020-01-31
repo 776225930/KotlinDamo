8b7d636a2a469fc227c1d0eaa516e2dc3f766f01
Jianghao <776225930@qq.com>
2020-01-31 15:45:27 +0800

    异常捕获

--- src/baseExercise/CalculatorDemo.kt
+++ src/baseExercise/CalculatorDemo.kt
@@ -6,13 +6,19 @@ package baseExercise
  * @describe
  */
 fun main(args: Array<String>) {
-    println("请出入一个数字")
-    val line1 = readLine()
-    println("请出入第二个数字")
-    val line2 = readLine()
+    while (true) {
+        println("请出入一个数字")
+        val line1 = readLine()
+        println("请出入第二个数字")
+        val line2 = readLine()
+        try {
+            var num1: Int = line1!!.toInt()
+            var num2: Int = line2!!.toInt()
+            println("${num1}+${num2}=${num1 + num2}")
+        } catch (e: Exception) {
+//        e.printStackTrace()
+            println("输入数据错误")
+        }
+    }
 
-    var num1: Int = line1!!.toInt()
-    var num2: Int = line2!!.toInt()
-
-    println("${num1}+${num2}=${num1 + num2}")
 }

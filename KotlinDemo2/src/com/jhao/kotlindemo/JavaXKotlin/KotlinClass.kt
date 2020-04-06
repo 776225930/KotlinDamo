369aed9fb1f467272a83d812a7d16b7bf976a6e7
BugMaker <776225930@qq.com>
2020-04-06 21:18:12 +0800

    Kotlin和Java的交互

---
+++ KotlinDemo2/src/com/jhao/kotlindemo/JavaXKotlin/KotlinClass.kt
@@ -0,0 +1,7 @@
+package com.jhao.kotlindemo.JavaXKotlin
+
+/**
+ * @date   2020/4/6
+ * @author JiangHao
+ * @describe
+ */
---
+++ KotlinDemo2/src/com/jhao/kotlindemo/JavaXKotlin/UserPresenter.java
@@ -0,0 +1,9 @@
+package com.jhao.kotlindemo.JavaXKotlin;
+
+/**
+ * @author JiangHao
+ * @date 2020/4/6
+ * @describe
+ */
+public class UserPresenter {
+}
